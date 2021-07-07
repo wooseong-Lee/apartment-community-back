042254287933d654f4dac06d8465486d95bdbcfa
jobis.lee <jobis.lee@kakaocorp.com>
2021-07-08 03:45:58 +0900

    Fix before shell script

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -6,9 +6,16 @@ fi
 
 mkdir -vp /home/ec2-user/deploy
 
-docker stop ldntjd1081/apt-server:latest
-docker rm ldntjd1081/apt-server:latest
+docker ps 2> /dev/null
 
-if [[ "$(docker images -q ldntjd1081/apt-server:latest 2> /dev/null)" != ""]]; then
+if [ "$(docker ps 2> /dev/null)" == *"ldntjd1081/apt-server"* ]; then
+  docker stop ldntjd1081/apt-server:latest
+fi
+
+if [ "$(docker ps -a 2> /dev/null)" == *"ldntjd1081/apt-server"* ]; then
+  docker rm -f ldntjd1081/apt-server:latest
+fi
+
+if [ "$(docker images -q ldntjd1081/apt-server:latest 2> /dev/null)" == *"ldntjd1081/apt-server"* ]; then
   docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference=ldntjd1081/apt-server:latest)
 fi
