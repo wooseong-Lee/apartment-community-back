e885f7772f4f329c773c8ab209a84be17bc098cb
jobis.lee <jobis.lee@kakaocorp.com>
2021-07-08 12:08:09 +0900

    Fix beforeInstall shell script

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -4,4 +4,14 @@ if [ -d /home/ec2-user/deploy ]; then
   rm -rf /home/ec2-user/deploy
 fi
 
-y | docker system prune -a
+if [ "$(docker ps -q)" != "" ]; then
+  docker stop `docker ps -q`
+fi
+
+if [ "$(docker ps -a -q)" != "" ]; then
+  docker rm -f `docker ps -a -q`
+fi
+
+if [ "$(docker images -q)" != "" ]; then
+  docker rmi -f `docker images -q`
+fi
